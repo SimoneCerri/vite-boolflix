6a7063119cc574b72dcd0aeb57f918dd2c37788a
SimoneCerri <simone.cerri1994@gmail.com>
2024-04-03 13:28:15 +0200

    fix removing the double ticks on link

--- src/store.js
+++ src/store.js
@@ -9,7 +9,7 @@ export const store = reactive
         callApi(ID, search) {
             console.log(this.search, this.ID);
 
-            axios.get(`https://api.themoviedb.org/3/search/movie?query="${this.search}"&api_key="${this.ID}"`)
+            axios.get(`https://api.themoviedb.org/3/search/movie?query=${this.search}&api_key=${this.ID}`)
                 .then(response => {
                     console.log(response.data);
                 })
