19a94e52336d5693dccaad0883a2085cb251b5b2
SimoneCerri <simone.cerri1994@gmail.com>
2024-04-03 11:50:28 +0200

    install axios, first try of the API call

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,19 +1,27 @@
 <script>
+import axios from "axios";
+
 export default
     {
         name: "Button",
+        props: ["search"],
         methods:
         {
-            callApi()
-            {
-                
+            callApi() {
+                axios.get("https://api.themoviedb.org/3/search/movie?query&api_key=25fe11ffdf5ce5a3da68cfa381752925")
+                    .then(response => {
+                        console.log(response.data);
+                    })
+                    .catch(err => {
+                        console.error(err.message) //log in console the error (if there are errors) just in case
+                    })
             }
         }
     }
 </script>
 
 <template>
-<button @click="callApi()">SEARCH</button>
+    <button @click="callApi()">SEARCH</button>
 </template>
 
 <style></style>
