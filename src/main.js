62031024d96fa59850d5e73fb82a792d8976c7e2
SimoneCerri <simone.cerri1994@gmail.com>
2024-04-03 18:06:40 +0200

    try to add Vue Flags

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
-import { createApp } from 'vue'
-import './assets/css/style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './assets/css/style.css';
+import App from './App.vue';
+import Vue from 'vue';
+import VueFlags from "@growthbunker/vueflags";
+Vue.use(VueFlags)
 
 createApp(App).mount('#app')
