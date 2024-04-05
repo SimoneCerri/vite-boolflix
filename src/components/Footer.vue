4c32433bae2111b9c845d4bb73d355cb86ef4cb6
SimoneCerri <simone.cerri1994@gmail.com>
2024-04-05 16:50:13 +0200

    add footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,14 +1,57 @@
 <script>
 export default
-{
-    name: "Footer",
-}
+    {
+        name: "Footer",
+    }
 </script>
 
 <template>
     <div id="footer">
-        <div class="left_footer">SX</div>
-        <div class="right_footer">DX</div>
+        <div class="left_footer">
+            <h4>
+                <strong>
+                    Create by:
+                </strong>
+            </h4>
+            <ul>
+                <li>
+                    <a href="#profile">
+                        Simone Cerri
+                    </a>
+                </li>
+                <li>
+                    <a href="joke_1">
+                        Google-search
+                    </a>
+                </li>
+                <li>
+                    <a href="joke_2">
+                        Discord
+                    </a>
+                </li>
+                <li>
+                    <a href="joke_3">
+                        ChatGPT
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="right_footer">
+            <h4>
+                <strong>
+                  Find us on:  
+                </strong>
+            </h4>
+            <ul>
+                <li><a href="#facebook"><i class="fa-brands fa-square-facebook" style="color: #ffffff;"></i></a>
+                </li>
+                <li><a href="#instagram"><i class="fa-brands fa-instagram" style="color: #ffffff;"></i></a></li>
+                <li><a href="#twitter"><i class="fa-brands fa-square-twitter" style="color: #ffffff;"></i></a></li>
+                <li><a href="#youtube"><i class="fa-brands fa-youtube" style="color: #ffffff;"></i></a></li>
+                <li><a href="#pinterest"><i class="fa-brands fa-square-pinterest" style="color: #ffffff;"></i></a>
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 
@@ -17,17 +60,50 @@ export default
     background-color: black;
     height: 120px;
     display: flex;
+    justify-content: space-evenly;
     align-items: center;
     text-align: center;
-    
-    .left_footer
+
+    h4
     {
-        flex-basis: 50%;
+        color: rgb(255, 145, 0);
+        margin-bottom: 1em
     }
 
-    .right_footer 
+    li
     {
-        flex-basis: 50%;
+        list-style: none;
+        a {
+            text-decoration: none;
+            color: white;
+            
+        }
+    }
+
+    .left_footer {
+        flex-basis: 20%;
+        
+        ul 
+        {
+            display: flex;
+            flex-direction: column;
+            
+        }
+    }
+
+    .right_footer {
+        flex-basis: 20%;
+        
+        ul 
+        {
+            display: flex;
+            justify-content: center;
+
+            li 
+            {
+                padding: 0.5em;
+            }
+        }
     }
 }
 </style>
